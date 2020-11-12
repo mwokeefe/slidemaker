dd139b5c0497f372c7139cee8f060931242454c0
Michael William O'Keefe <michael.william.o'keefe@cern.ch>
2020-11-12 11:06:30 +0000

    sshpass to rsync

--- src/compile.sh
+++ src/compile.sh
@@ -7,7 +7,8 @@ mkdir -p images
 
 if [ $# -ne 0 ]
 then
-    scp -T miokeefe@lxplus.cern.ch:"/eos/user/m/miokeefe/ZcountingPlots/FullRun2/BCID/*8*.eps" images
+    sshpass -p $(pass lxplus) rsync miokeefe@lxplus.cern.ch:"/eos/user/m/miokeefe/ZcountingPlots/FullRun2/*all*.pdf" images/
+    rsync mokeefe@gateway.ph.liv.ac.uk:"" images/
 fi
 
 pandoc markdown_beamer_example.md -t beamer -o ${DATE}_${TAG}.pdf --slide-level 2 --include-in-header style/my_style.tex
