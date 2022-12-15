0d3d2bc9915e573a0f43f5ac50a2ae455bada0ad
Michael O'Keefe <michael.o'keefe@raytheon.com>
2022-12-15 09:56:06 +0000

    Moved compile into src

--- src/compile.sh
+++ src/compile.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-DATE=2020_xx_yy
-TAG=OutputName
+DATE=YYY_MM_DD
+TAG=ExampleDocTitle
 
 mkdir -p images 
 
+get_image() {
+    wget -nc $1 -P ./images/
+}
+
 if [ $# -ne 0 ]
 then
-    sshpass -p $(pass lxplus) rsync miokeefe@lxplus.cern.ch:"/eos/user/m/miokeefe/ZcountingPlots/FullRun2/*all*.pdf" images/
-    #rsync mokeefe@gateway.ph.liv.ac.uk:"" images/
-    notify-send -i ./style/tick.png "SlideMaker" "Images downloaded. Compiling..."
+    get_image http://cds.cern.ch/record/2678463/files/fig_01a-eps-converted-to.png
 fi
 
 pandoc markdown_beamer_example.md -t beamer -o ${DATE}_${TAG}.pdf --slide-level 2 --include-in-header style/my_style.tex --variable urlcolor=steelblue --variable linkcolor=white
-notify-send -i ./style/tick.png "SlideMaker" "Slides compiled!"
